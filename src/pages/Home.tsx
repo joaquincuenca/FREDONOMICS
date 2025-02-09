434f16d96b5584e8c415dc52a75ff44763c428ab
finnsatoshi03 <finnsatoshi03@gmail.com>
2025-02-09 18:32:48 +0800

    update Home component layout for improved responsiveness; add family image for enhanced visual appeal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="h-full w-full relative flex md:grid xl:grid-cols-[1fr_0.3fr] flex-col gap-6">
       <div className="relative md:grid md:grid-cols-[auto_1fr] flex flex-col gap-4 md:gap-6">
-        <div className="md:h-full lg:absolute left-0 grid grid-rows-[auto_1fr]">
+        <div className="md:h-full lg:absolute left-0 grid grid-rows-[auto_1fr] sm:gap-8">
           <div className="hidden lg:block">
             <h1 className="text-6xl uppercase title-h1-p1 relative w-fit rounded-br-3xl bg-white lg:pr-6 lg:pb-2 text-black">
               Build Wealth That Lasts
@@ -23,6 +23,11 @@ export default function Home() {
           <h1 className="uppercase text-5xl lg:hidden">
             Build Wealth That Lasts for Generations
           </h1>
+          <img
+            src="/images/family.jpg"
+            alt="Family in a field"
+            className="rounded-xl object-cover h-full w-full hidden sm:block lg:hidden"
+          />
         </div>
         <div className="md:space-y-8 flex gap-14 flex-col justify-between bg-white w-fit lg:mt-[50%] title-content relative">
           <div className="space-y-4">
@@ -40,7 +45,7 @@ export default function Home() {
               Get Your Free Consultation
             </Button>
           </div>
-          <div className="pb-8">
+          <div className="lg:pb-8">
             <CardStack items={CARDS} />
           </div>
         </div>
